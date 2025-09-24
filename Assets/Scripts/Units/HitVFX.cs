44b0653660cff211195f5958666005db66b0bd53
Huynh Tran Khanh Quynh <quynh190504@gmail.com>
2025-09-24 15:06:49 +0700

    Rename HitEffect to HitVFX

--- Assets/Scripts/Units/HitVFX.cs
+++ Assets/Scripts/Units/HitVFX.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class HitEffect : MonoBehaviour
+public class HitVFX : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
